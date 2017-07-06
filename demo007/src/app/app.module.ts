2cbe7e65cef3416d82f4363c658738acbc8903a3
jiekekeji <jieke_rose@foxmail.com>
2017-07-06 08:46:18 +0800

    AngularJS2-基本的项目模板，省去每次重新构建项目，每次重新下载依赖，直接拷贝使用

--- demo007/src/app/app.module.ts
+++ demo007/src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+
+import {appRoutes}    from "./app.routes";
+import {AppComponent} from './app.component';
+import {IndexComponent} from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
