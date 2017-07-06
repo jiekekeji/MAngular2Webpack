fb0791de34419449fa2c892059d3d41ba96b9447
jiekekeji <jieke_rose@foxmail.com>
2017-07-06 17:02:38 +0800

    AngularJS2-模板语法、数据绑定

--- demo008/README.md
+++ demo008/README.md
@@ -1,2 +1,74 @@
-AngularJS2-数据绑定
+AngularJS2-模板语法、数据绑定
 ====
+
+管道可以理解为过滤器：从一头把东西放入，经过过滤后，出来新的东西。简单的可以理解为函数，传入参数处理后返回结果值。
+
+1、如何自定义管道及使用。这里创建一个字母大小写转换的管道：
+------- 
+
+第一步：在app/pipe目录下新建管道letterpipe.pipe.ts，使用新建管道命令：
+```
+ng g p pipe/letterpipe
+
+```
+命令运行完成后在app/pipe目录下可看到letterpipe.pipe.ts文件，同时在app.module.ts文件中已引入并声明了该管道.
+app.module.ts:
+```
+import {LetterpipePipe} from './pipe/letterpipe.pipe';
+  ...
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    LetterpipePipe,
+  ],
+  ...
+```
+letterpipe.pipe.ts文件初始内容如下：
+```
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'letterpipe'
+})
+export class LetterpipePipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+    return null;
+  }
+
+}
+
+```
+其中@Pipe({name: 'letterpipe'})的name值为该管道的标识，通过该名字使用管道。
+
+第二步：实现管道的transform方法：
+```
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: 'letterpipe'
+})
+export class LetterpipePipe implements PipeTransform {
+
+  /**
+   * 字母大写小的转换
+   * @param value   需要转换的字母
+   * @param isToLocaleUpperCase 1转为大写；否则转为小写
+   * @returns {string}  转换后的值
+   */
+  transform(value: string, isToLocaleUpperCase: string): any {
+    console.log(isToLocaleUpperCase);
+    if (isToLocaleUpperCase === "1") {
+      return value.toLocaleUpperCase();
+    } else {
+      return value.toLocaleLowerCase();
+    }
+  }
+}
+```
+第三步：使用管道，在index.component.html中将输入的字母转为大写或小写：
+```
+<p>使用管道转换后的结果:{{letter|letterpipe:isUpper}}</p>
+```
+其中letter对应函数transform的参数value;letterpipe为管道的名称;isUpper为函数transform的参数isToLocaleUpperCase。
+
--- demo008/src/app/index/index.component.ts
+++ demo008/src/app/index/index.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +7,112 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  private username = "张三";
+  private student = {
+    "username": "小丽",
+    "age": 18,
+    "address": "地球村"
+  };
+
+  private student1 = {
+    "username": "小明",
+    "age": 11,
+    "address": "月亮"
+  };
+  private student2 = {
+    "username": "小红",
+    "age": 14,
+    "address": "火星"
+  };
+  private student3 = {
+    "username": "小白",
+    "age": 20,
+    "address": "天王星"
+  };
+
+  private students = [this.student1, this.student2, this.student3];
+
+  /************************************/
+
+  private imgurl = "http://imgsrc.baidu.com/imgad/pic/item/9d82d158ccbf6c81506cc9cbb63eb13533fa40be.jpg";
+  private imgH = 100;
+
+
+  private txColor = "red";
+
+  private styleColor = {
+    "color": "blue",
+    "font-size": "20px",
+    "background-color": "gainsboro",
+  };
+
+  /***********************************/
+
+  private isCcsty = true;
+
+  private moreClz = {
+    "ccsty": true,
+    "aasty": true
+  }
+
+  /***********************************/
+  private isShow = true;
+
+  private caseV = 1;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  getImgW() {
+    return 100;
+  }
+
+  getTxColor() {
+    return "red"
+  }
+
+  getStyleColor() {
+    return {
+      "color": "blue",
+      "font-size": "20px",
+      "background-color": "gainsboro",
+    };
+  }
+
+  /***********************************/
+
+  getIsCcsty() {
+    return false;
+  }
+
+  getMoreClz() {
+    return {
+      "ccsty": false,
+      "aasty": true
+    }
+  }
+
+  /***********************************/
+  changeIsShow() {
+    if (this.isShow) {
+      this.isShow = false;
+    } else {
+      this.isShow = true;
+    }
+  }
+
+  changeCaseV() {
+    if (this.caseV === 3) {
+      this.caseV = 1;
+    } else {
+      this.caseV++;
+    }
+  }
+
+  itemClick(item) {
+    console.log("列表事件绑定:", item);
+  }
 }
